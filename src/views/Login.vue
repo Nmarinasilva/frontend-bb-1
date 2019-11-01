1242cb161018dd636352a4c50d89c3cb0ffb8d18
Christian Toledo <christian.odt@gmail.com>
2019-11-01 15:02:40 -0300

    initial commit

---
+++ src/views/Login.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="row justify-content-center">
+    <div class="col-lg-5 col-md-7">
+      <div class="card bg-secondary shadow border-0">
+        <div class="card-body px-lg-5 py-lg-5">
+          <div class="text-center text-muted mb-4">
+            <big>Entrar</big>
+          </div>
+          <form role="form">
+            <base-input
+              class="input-group-alternative mb-3"
+              placeholder="Email"
+              addon-left-icon="ni ni-email-83"
+              v-model="model.email"
+            ></base-input>
+
+            <base-input
+              class="input-group-alternative"
+              placeholder="Senha"
+              type="password"
+              addon-left-icon="ni ni-lock-circle-open"
+              v-model="model.password"
+            ></base-input>
+
+            <!-- <base-checkbox class="custom-control-alternative">
+              <span class="text-muted">Remember me</span>
+            </base-checkbox>-->
+            <div class="text-center">
+              <base-button type="primary" class="my-4">Entrar</base-button>
+            </div>
+          </form>
+        </div>
+      </div>
+      <div class="row mt-3">
+        <div class="col-12 text-center">
+          <router-link to="/register" class="text-primary">
+            <p>Create new account</p>
+          </router-link>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: "login",
+  data() {
+    return {
+      model: {
+        email: "",
+        password: ""
+      }
+    };
+  }
+};
+</script>
+<style>
+</style>
---
+++ src/views/Register.vue
@@ -0,0 +1,79 @@
+<template>
+  <div class="row justify-content-center">
+    <div class="col-lg-5 col-md-7">
+      <div class="card bg-secondary shadow border-0">
+        <div class="card-body px-lg-5 py-lg-5">
+          <div class="text-center text-muted mb-4">
+            <big>Insira seus dados</big>
+          </div>
+          <form role="form">
+            <base-input
+              class="input-group-alternative mb-3"
+              placeholder="Nome"
+              addon-left-icon="ni ni-hat-3"
+              v-model="model.name"
+            ></base-input>
+
+            <base-input
+              class="input-group-alternative mb-3"
+              placeholder="CPF"
+              addon-left-icon="ni ni-hat-3"
+              v-model="model.cpf"
+            ></base-input>
+
+            <base-input
+              class="input-group-alternative mb-3"
+              placeholder="Telefone"
+              addon-left-icon="ni ni-hat-3"
+              v-model="model.tel"
+            ></base-input>
+
+            <base-input
+              class="input-group-alternative mb-3"
+              placeholder="Cargo"
+              addon-left-icon="ni ni-hat-3"
+              v-model="model.cargo"
+            ></base-input>
+            <hr />
+            <base-input
+              class="input-group-alternative mb-3"
+              placeholder="Email"
+              addon-left-icon="ni ni-email-83"
+              v-model="model.email"
+            ></base-input>
+
+            <base-input
+              class="input-group-alternative"
+              placeholder="Password"
+              type="password"
+              addon-left-icon="ni ni-lock-circle-open"
+              v-model="model.password"
+            ></base-input>
+            <div class="text-center">
+              <base-button type="primary" class="my-4">Cadastrar</base-button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: "register",
+  data() {
+    return {
+      model: {
+        name: "",
+        cpf: "",
+        tel: "",
+        cargo: "",
+        email: "",
+        password: ""
+      }
+    };
+  }
+};
+</script>
+<style>
+</style>
